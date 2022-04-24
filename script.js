2c093adf28df8dedb27d26a1c42dd1a3be6dd9c5
oocee <ossi.takkinen@gmail.com>
2022-04-24 18:03:24 +0300

    upgrade

--- script.js
+++ script.js
@@ -13,12 +13,8 @@ function loadTheatre() {
         if (xhr.status == 200) {
             xmlDoc = xhr.responseXML;
             show = xmlDoc.getElementsByTagName('Show');
-            //console.log(show);
-            //console.log(show.length);
-            //console.log(show[0].childNodes[1].innerHTML);
-            //console.log(show[0].children.length);
-            //console.log(show[0].children[1].nodeName);
             var tID = null;
+            delete1();
             for (var i = 0; i < show.length; i++) {
                for (var a = 0; a < show[i].children.length; a++) {
                    var str = show[i].children[a].nodeName;
@@ -26,8 +22,29 @@ function loadTheatre() {
                    if (str.includes('TheatreID')) {
                         tID = show[i].children[a].innerHTML;
                         // console.log(tID);
+                        //console.log(show[i].children[a].children.length);
                         if (theatreId == tID) {
-                            console.log(show[i].children[15].innerHTML);
+                            // console.log(show[i].children[15].innerHTML);
+                            document.getElementById("shows").innerHTML += 
+                            "<div id='frame'>"
+                                +"<div id='movie'>" 
+                                    + moviename(i) 
+                                +"</div>"
+                                +"<div id='img'>" 
+                                    + "<img src='" + image(i) +"' width='100'> </img>"
+                                +"</div>"
+                                +"<div id='img2'>" 
+                                    + "<img src='" + rating(i) +"' width='50' id='img3'> </img>"
+                                +"</div>"
+                                +"<div id='frameTime'>"
+                                    +"<div id='time'>" 
+                                        + "Show stars at " + startingTime(i) 
+                                    +"</div>"
+                                    +"<div id='auditorium'>" 
+                                        + "Auditorium: "+ auditorium(i) 
+                                    +"</div>"
+                                +"</div>"
+                            +"</div>";
                        }
                    }
                }
@@ -35,11 +52,56 @@ function loadTheatre() {
         }
          
     }
-    
- 
-    
-    //console.log(xmlDoc.getElementsByTagName('TheatreArea')[0].getAttribute('ID'));
+    function moviename(i) {
+        return show[i].children[15].innerHTML;
+    }
+    function image(i) {
+        var b = null;
+        var d = null;
+        for (a = 0; a < show[i].children.length; a++) {
+            if (show[i].children[a].nodeName.includes('Images')) {
+                b = a;
+                console.log(show[i].children[a].children.length);
+                    for (c = 0; c < show[i].children[a].children.length; c++){
+                        if (show[i].children[a].children[c].nodeName.includes('EventLargeImagePortrait')) {
+                        d = c;
+                        }
+                    }
+                }
+            }
+        return show[i].children[b].children[d].innerHTML;
+    }
+    function rating(i) {
+        var b = null;
+            for (a = 0; a < show[i].children.length; a++) {
+                if (show[i].children[a].nodeName.includes('RatingImageUrl')) {
+                    b = a;
+                }
+             }
+        return show[i].children[b].innerHTML;
+    }
+    function startingTime(i) {
+        var b = null;
+            for (a = 0; a < show[i].children.length; a++) {
+                if (show[i].children[a].nodeName.includes('dttmShowStart') && !show[i].children[a].nodeName.includes('UTC')) {
+                    b = a;
+                }
+             }
+        var time = show[i].children[b].innerHTML;
+        var timex = time.substr(11, 5);  
+        return timex;      
+    }
+    function auditorium(i) {
+        var b = null;
+            for (a = 0; a < show[i].children.length; a++) {
+                if (show[i].children[a].nodeName.includes('TheatreAuditorium')) {
+                    b = a;
+                }
+             }
+        return show[i].children[b].innerHTML;        
+    }
 }
-
-
+function delete1() {
+    document.getElementById("shows").innerHTML = " ";
+}
 
